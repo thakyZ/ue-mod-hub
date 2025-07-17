b006bc527e4719cd19a1d40a905dc24ef15f853b
thakyZ <950594+thakyZ@users.noreply.github.com>
2025-07-17 11:32:24 -0700

    add: typing information to Object interface

--- types/global.d.ts
+++ types/global.d.ts
@@ -6,6 +6,7 @@ import type {
     ServerCacheBinder,
     UStoreBinder,
 } from '@main/preload';
+import type { ValueOf } from 'type-fest'
 
 declare global {
     interface Window {
@@ -15,5 +16,14 @@ declare global {
         palhub: PalHubBinder;
         serverCache: ServerCacheBinder;
         uStore: UStoreBinder;
+    }
+    interface Array<T> {
+        // sort(): Array<T>;
+    }
+    interface ObjectConstructor {
+        keys<T, TKey extends keyof T = keyof T>(obj: T): Array<TKey>;
+        values<T, TValue extends ValueOf<T> = ValueOf<T>>(obj: T): Array<TValue>;
+        entries<T, TKey extends keyof T = keyof T, TValue extends ValueOf<T, TKey> = ValueOf<T, TKey>>(obj: T): Array<[key: TKey, value: TValue]>;
+        fromEntries<T, TKey extends keyof T = keyof T, TValue extends ValueOf<T, TKey> = ValueOf<T, TKey>>(obj: Array<[key: TKey, value: TValue]>): T;
     }
 }
